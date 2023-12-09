9eb562601a75ea2fb49a5c4678dff486176c0167
Yordan Andronov <netherbull@gmail.com>
2023-12-09 21:06:39 +0200

    Update __init__.py

---
+++ med_assist_app/modules/doctor/__init__.py
@@ -0,0 +1,13 @@
+from .doctor import(
+    create_doctor_table,
+    get_day_of_week,
+    save_doc_info,
+    get_available_hours,
+    get_doctors_by_specialty_and_city,
+    get_doctor_profile,
+    get_available_hours_excluding_booked,
+    get_doctor_appointments,
+)
+from .schedule import(
+    create_doctor_schedule_table,
+)
