be5a7ae4e5747f41f67669d5518ace6f696eb94b
addie230 <adrianamaslarova23@gmail.com>
2023-12-09 20:56:51 +0200

    Update schedule.py

---
+++ med_assist_app/modules/doctor/schedule.py
@@ -0,0 +1,22 @@
+def create_doctor_schedule_table():
+    try:
+        dc_schedule = sqlite3.connect(r'C:\Users\MSI\Desktop\MedAssist\user_profiles.db')
+        cursor = dc_schedule.cursor()
+
+        cursor.execute('''
+            CREATE TABLE IF NOT EXISTS doctor_schedule (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                doctor_username TEXT NOT NULL,
+                day TEXT,
+                available_hours TEXT,  -- Store the list as a JSON string
+                start_time TEXT,
+                end_time TEXT,
+                UNIQUE(doctor_username, day)
+            )
+        ''')
+
+        dc_schedule.commit()
+        dc_schedule.close()
+        print("Doctor schedule created successfully.")
+    except sqlite3.Error as e:
+        print("Error creating table:", e)
